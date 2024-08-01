c8884b2a946b893d7a65fd2341a5192d55a197ab
Siddharth Singh <sidd6.10@gmail.com>
2024-08-01 15:45:05 +0530

    commit

---
+++ src/DataEmployee.js
@@ -0,0 +1,32 @@
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName: 'Mark',
+        lastName: 'David',
+        age: 20
+    },
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        age: 25
+    },
+    {
+        id: 3,
+        firstName: 'John',
+        lastName: 'Smith',
+        age: 30
+    },
+    {
+        id: 4,
+        firstName: 'Emily',
+        lastName: 'Jones',
+        age: 22
+    },
+    {
+        id: 5,
+        firstName: 'Michael',
+        lastName: 'Brown',
+        age: 28
+    }
+];
